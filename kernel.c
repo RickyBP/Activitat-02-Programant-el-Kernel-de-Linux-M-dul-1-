b389fdd74b82db737ce8ed70aa72180ed4a8ef59
JoanBonell <100761163+JoanBonell@users.noreply.github.com>
2022-10-16 22:20:49 +0200

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -2,8 +2,8 @@
     int main(int argv, char **argc){
         int *sys_call_table = (int *) arch/x86/entry/calls/syscalls/syscall_65.tbl; //Address of sys_call_table
         int *sys_chmod = (int *) 90; //sys_chmod
-        int *printk = (int *) 0x000e8a60; //We can use the same address of sys_chmod
-        int *string = (int *) 0x0804a0a0; //String de "CHMOD is drunk"
+        int *printk = (int *) 345; //We can use the same address of sys_chmod
+        int *string = (int *) 346; //String de "CHMOD is drunk"
         int *chmod = (int *) 345; //Address of chmod
         sys_call_table[90] = (int) printk; //We change the address of chmod for the address of printk
         printk(string); //We print the string "CHMOD is drunk"
