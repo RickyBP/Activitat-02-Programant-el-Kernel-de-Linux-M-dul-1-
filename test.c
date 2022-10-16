4d3e8c294cc96f2989d1408e00040f3520aff828
JoanBonell <100761163+JoanBonell@users.noreply.github.com>
2022-10-16 22:24:55 +0200

    Add files via upload

---
+++ test.c
@@ -0,0 +1,24 @@
+//Realitzeu la implemetació, integració i test d'una nova crida a sistema anomenada gettimes. gettimes(int, pid, struct ptimes *pt).
+//Aquesta crida ens permetrà obtenir informació sobre un procés concret:
+//real: temps total del procés.
+//user: temps total en mode usuari.
+//sys: temps total en mode sistema.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+
+#define __NR_gettimes 333
+
+
+
+int main(int argc, char *argv[])
+{
+    gettimes(argv[0], &pt);
+    printf("start_time: %lu \t real_time: %lu \t user_time: %lu \t sys_time: %lu \n", pt.start_time, pt.real_time, pt.user_time, pt.sys_time);
+    return 0;
+}
